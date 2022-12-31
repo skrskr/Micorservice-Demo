e931e743a6f6143eb617e5216cdeff594d25d193
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2022-12-31 14:16:27 +0200

    added frontend rest api migrations

--- front_api/main.py
+++ front_api/main.py
@@ -1,6 +1,35 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:root@db/main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+    
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name="user_product_unique")
+
 
 @app.route('/')
 def index():
---
+++ front_api/manager.py
@@ -0,0 +1,13 @@
+from main import db, app
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
